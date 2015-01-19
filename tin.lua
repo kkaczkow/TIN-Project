78079034b964284ccdc9d179add460968498b4fe
Michal Kita <kitus20@gmail.com>
2015-01-19 19:20:36 +0100

    Changes in LUA

--- tin.lua
+++ tin.lua
@@ -1,30 +1,27 @@
 -- najprosciej dodac ten skrypt wywolujac wireszarka w nastepujacy sposob:
 -- sudo wireshark -X lua_script:'/<home_dir>/tin.lua'
 
-p_tindrive= Proto("tindrive", "TinDrive Protocol")
+p_repoagent3000= Proto("repoagent3000", "RepoAgent3000 Protocol")
 local data_data = Field.new("data.data")
-local td_type = ProtoField.string("tindrive.type", "Packet type", ftypes.STRING)
-local td_data = ProtoField.bytes("tindrive.data", "Packet data")
-p_tindrive.fields._type = td_type
-p_tindrive.fields._data = td_data
+local td_type = ProtoField.string("repoagent3000.type", "Packet type", ftypes.STRING)
+local td_data = ProtoField.bytes("repoagent3000.data", "Packet data")
+p_repoagent3000.fields._type = td_type
+p_repoagent3000.fields._data = td_data
 
 local ssl_dissector = Dissector.get("ssl")
 
-function p_tindrive.dissector(buffer, pinfo, tree)
-	ssl_dissector:call(buffer, pinfo, tree)
+function p_repoagent3000.dissector(buffer, pinfo, tree)
 	pinfo.cols.protocol = "TIN"
-	local subtree = tree:add(p_tindrive, buffer(), "TinDrive Protocol Data")
+	local subtree = tree:add(p_repoagent3000, buffer(), "RepoAgent3000 Protocol Data")
 	if data_data() then
 		local data_arr = { data_data() }
 		local data = ByteArray.new()
 		for k, v in ipairs(data_arr) do
 			data:append(v())
 		end
-		local data_tvb = ByteArray.tvb(data, "TinDrive Raw Data")
-		local packet_type = ""
+		local data_tvb = ByteArray.tvb(data, "RepoAgent3000 Raw Data")
+		local packet_type = "UNDEFINED"
 		if data_tvb[1] == 0x01 then
-			packet_type = 0x16
-		elseif data_tvb[1] == 0x01 then
 			packet_type = "REGISTER"
 		elseif data_tvb[1] == 0x81 then
 			packet_type = "REGISTERED_ID"
@@ -48,4 +45,4 @@ function p_tindrive.dissector(buffer, pinfo, tree)
 	end
 end
 
-DissectorTable.get("tcp.port"):add(9000, p_tindrive)
+DissectorTable.get("tcp.port"):add(9000, p_repoagent3000)
